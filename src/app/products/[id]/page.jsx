a881ba3e6181b545e01e5b325e02b4c85d227a08
theiguim <theiguim@gmail.com>
2024-08-19 18:35:09 -0300

    getStaticParams

--- src/app/products/[id]/page.jsx
+++ src/app/products/[id]/page.jsx
@@ -1,18 +1,24 @@
 import ProductDetailer from "@/components/ProductDetailer/page";
 import styles from "../../../styles/productsStyle.module.scss";
-import { fetchProduct } from "@/service/products";
+import { fetchProduct, fetchProducts } from "@/service/products";
+
+export const generateStaticParams = async () => {
+
+  const products = await fetchProducts();
+  return products.map(product => ({
+    id: product.id.toString()
+  }));
+};
 
 
+const Product = async ({ params }) => {
 
+  const { id } = params;
+  const product = await fetchProduct(id);
 
-const Product = async ({params}) =>{
-
-    const {id} = params;
-    const product = await fetchProduct(id);
-
-return (
-  <ProductDetailer product={product} />
-);
+  return (
+    <ProductDetailer product={product} />
+  );
 };
 
 export default Product;
